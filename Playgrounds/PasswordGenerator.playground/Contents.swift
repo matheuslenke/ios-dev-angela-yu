c4415a0579771a9f2ba035a723429dd80b14da21
Matheus Lenke <matheus.l.coutinho@aluno.ufes.br>
2021-04-29 10:52:04 -0300

    feat: Magic 8 ball app

---
+++ Playgrounds/PasswordGenerator.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+func exercise() {
+    
+    //The number of letters in alphabet equals 26
+    let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    
+    // 🚨 Don't change the code above 👆
+    //####################################
+    //Write your code below this line 👇
+    
+    let password = alphabet.shuffle() //Replace this comment with your code.
+
+
+
+
+    //Write your code above this line 👆
+    //####################################
+
+    //🚨 Don't change the code below 👇
+    print(password)
+    
+    
+}
+
+exercise()
+    
---
+++ Playgrounds/SwiftVariables.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var str = "Hello, playground"
+
+let int = 2
+print("Integer: ", int)
+
+let float = 2.5
+print("Float: ", float)
+
+let boolean = true
+
+print("Boolean: ", boolean)
